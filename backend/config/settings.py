32acbbf3dbc1a12b0e8265f125573014a7e1241b
Nonso <yungmartins172gmail.com>
2026-08-02 22:25:46 +0100

    edited settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -224,3 +224,5 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
