05f36f1a3c43d8a1671f9ee48e988451bf17cb55
Nonso <yungmartins172gmail.com>
2026-08-03 10:29:20 +0100

    test

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -171,7 +171,7 @@ USE_TZ = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://your-shoekave.vercel.app",
+    "https://shoekave.vercel.app",
 ]
 
 from datetime import timedelta
